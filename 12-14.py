7cc269493e116a2761cc1b0807f99814b5772f36
Em2510 <sokem1025@gmail.com>
2023-04-03 13:32:51 +0200

    kolejne zmiany

--- 12-14.py
+++ 12-14.py
@@ -1,8 +1,8 @@
 class Transformer():
     
-    def transform():
-        pass
+    def transform(self,x_param,y_param):
+        return x_param+y_param
 
 def transform2(a,b):
-    x=a+b
+    x=2*(a+b)
     rturn(x)
